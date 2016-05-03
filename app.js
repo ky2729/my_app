fc30fed7cf2e121c775e47a1e008bf79c7fd0299
ky2729 <ky352729@naver.com>
2016-05-03 11:47:22 +0900

    정적파일추가

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
-app.get("/", function (req, res) {
-  res.send("<h1>Hello! World!</h1>");
-});
 
+app.use(express.static(path.join(__dirname,'/public')));
 
 
 app.listen(4000, function(){ console.log("Server On!");});
