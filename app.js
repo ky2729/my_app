46fa029e2f60c508a231f111f7b4c03ef6ca3321
ky2729 <ky352729@naver.com>
2016-05-03 11:23:40 +0900

    서버 추가

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello! World!</h1>");
+});
+
+
+
+app.listen(4000, function(){ console.log("Server On!");});
